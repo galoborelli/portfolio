28ba91e116d96613d59ca928ac2a06f649e9de1a
galoborelli <borelligalo19@gmail.com>
2025-10-28 08:58:56 +0100

    adding style

--- src/Sections/Presentation/index.jsx
+++ src/Sections/Presentation/index.jsx
@@ -38,8 +38,8 @@ function Presentation({ media , data}) {
           component="img"
           src={media[7].url}
           sx={{
-            width: { lg: "400px", xs: "200px" },
-            height: { lg: "400px", xs: "200px" },
+            width: { lg: "300px", xs: "200px" },
+            height: { lg: "300px", xs: "200px" },
             objectFit: "cover",
             borderRadius: "50%",
           }}
